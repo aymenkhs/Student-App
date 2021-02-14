d5c96124115530a5303fbbc1dc37308b8f346bbc
Anouar Aissani <aaissani571@gmail.com>
2021-02-14 15:53:10 +0100

    Added calendar table

--- mobile_frontend/lib/menu_elements/calendar.dart
+++ mobile_frontend/lib/menu_elements/calendar.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_frontend/main_drawer.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class Calendar extends StatefulWidget {
   @override
@@ -7,8 +10,44 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  final CalendarController _controller = CalendarController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Calendar',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        backgroundColor: Color(0xff43B485),
+        elevation: 0.0,
+      ),
+      drawer: MainDrawer(),
+      body: TableCalendar(
+        calendarController: _controller,
+        //initialCalendarFormat: CalendarFormat.week,
+        calendarStyle: CalendarStyle(
+          weekendStyle: TextStyle(color: Colors.black),
+          //outsideDaysVisible: false,
+          todayColor: Colors.lightBlueAccent,
+          selectedColor: Theme.of(context).primaryColor,
+          todayStyle: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 18.0, color: Colors.white),
+        ),
+        headerStyle: HeaderStyle(
+          leftChevronIcon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          rightChevronIcon: Icon(Icons.arrow_forward_ios, color: Colors.white),
+          titleTextStyle: TextStyle(color: Color(0xff43B485), fontSize: 20),
+          formatButtonVisible: false,
+          //: IconButton(icon: false,)
+        ),
+        startingDayOfWeek: StartingDayOfWeek.sunday,
+        weekendDays: [DateTime.friday],
+      ),
+    );
   }
 }
